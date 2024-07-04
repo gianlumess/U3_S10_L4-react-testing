7598abb32ce322d3981b63f2b9691f6181c77795
Gianluca Messina <gianluca.messina8201@gmail.com>
2024-07-04 15:30:50 +0200

    test montaggio del componente Welcome passato

--- src/tests/Welcome.test.jsx
+++ src/tests/Welcome.test.jsx
@@ -5,7 +5,7 @@ import Welcome from "../components/Welcome";
 describe("mounts of Welcome component", () => {
   it("mounts Alert correctly", () => {
     render(<Welcome />);
-    expect(alert).toBeInTheDocument();
     const alert = screen.getByRole("alert");
+    expect(alert).toBeInTheDocument();
   });
 });
