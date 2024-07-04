0c179966d6b449741766a35f90c861b4e601c46d
Gianluca Messina <gianluca.messina8201@gmail.com>
2024-07-04 16:56:28 +0200

    singleComment test wip

---
+++ src/tests/SingleComment.test.jsx
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import SingleComment from "../components/SingleComment";
+
+describe("when the page start and there's no click in any card the singlecomment component shouldn't render");
+it("not render comment if there's no click at the start", () => {
+  render(<SingleComment />);
+  const listItems = screen.queryAllByRole("listitem");
+  expect(listItems).toHaveLenght(0);
+});
